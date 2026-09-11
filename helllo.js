163c7172707c90420ca48a48652f4c6ea8a1ff8a
RohitKeshavala <tatva.pct29@outlook.com>
2026-09-11 16:12:14 +0530

    Duplicated data

--- helllo.js
+++ helllo.js
@@ -1,2 +1,8 @@
 const hello = "Hello, World!";
 const hiii = "Hii, World!";
+const hello1 = "Hello, World!";
+const hiii1 = "Hii, World!";
+const hello2 = "Hello, World!";
+const hiii2 = "Hii, World!";
+const hello3 = "Hello, World!";
+const hiii3 = "Hii, World!";
